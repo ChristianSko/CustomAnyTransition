727e267340ce2d77a677716f04e47729a3d3facd
Christian Skorobogatow <cskorobogatow@deloitte.es>
2022-08-02 13:54:12 +0200

    Initial Commit

---
+++ CustomAnyTransition/CustomAnyTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAnyTransitionApp.swift
+//  CustomAnyTransition
+//
+//  Created by Skorobogatow, Christian on 2/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CustomAnyTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
